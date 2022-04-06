4912c3539596e700ca1f6a20af5fea1be7cda743
Amr.Mohamed <amrmohamedms83@gmail.com>
2022-04-06 17:44:50 +0200

    fixes

--- src/app/modules/characters/characters/characters.component.ts
+++ src/app/modules/characters/characters/characters.component.ts
@@ -7,11 +7,10 @@ import { CharacterService } from 'src/app/services/character.service';
 import { MediaService } from 'src/app/services/media.service';
 import { TagsService } from 'src/app/services/tags.service';
 import { environment } from 'src/environments/environment';
-import SwiperCore, { Autoplay, EffectCoverflow, EffectFade, Swiper, SwiperOptions } from 'swiper';
+import SwiperCore, { Autoplay, EffectCoverflow } from 'swiper';
 import { SwiperComponent } from 'swiper/angular';
 
-// SwiperCore.use([Autoplay, EffectCoverflow]);
-SwiperCore.use([EffectCoverflow]);
+SwiperCore.use([Autoplay, EffectCoverflow]);
 
 @Component({
 	selector: 'app-characters',
@@ -19,7 +18,6 @@ SwiperCore.use([EffectCoverflow]);
 	styleUrls: ['./characters.component.scss']
 })
 export class CharactersComponent implements OnInit {
-	@ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
 	public characters: Character[] = [];
 	public selectedChar: Character | undefined;
 	public imgApi: string = environment.imgApi;
@@ -44,32 +42,13 @@ export class CharactersComponent implements OnInit {
 		this.characterService.getCharacters().then(res => {
 			this.characters = res;
 
-			let request: any = [];
-			for (let index = 0; index < this.characters.length; index++) {
-				const element = this.characters[index];
-				element.order = index + 1;
-				if (element.id) {
-					request.push(this.mediaService.GetByCharacterAndTags({ 'CharacterID': Number(element.id), 'TagIds': [12] }))
-					// this.mediaService.GetByCharacterAndTags({ 'CharacterID': Number(element.id), 'TagIds': [12] }).then(data => {
-					// 	let _charMedia: Media[] = data;
-					// 	for (let i = 0; i < _charMedia.length; i++) {
-					// 		const mediaElement = _charMedia[i];
-					// 		if (mediaElement.id) {
-					// 			element.profileURL = `${this.imgApi}/Media/GetMedia?id=${mediaElement.id}`
-					// 			element.mediaID = mediaElement.id
-					// 			this.teachers.push(element)
-					// 			this.teachers = this.sortArray(this.teachers)
-					// 		}
-					// 	}
-					// })
-				}
-			}
+			let request: Promise<Media[]>[] = [];
+			request = this.characters.map(CH => {return this.mediaService.GetByCharacterAndTags({ 'CharacterID': Number(CH.id), 'TagIds': [12] })})
 			forkJoin(request).subscribe((res: any[]) => {
 				for (let index = 0; index < res.length; index++) {
 					const element: any[] = res[index];
 					for (let i = 0; i < element.length; i++) {
 						const media = element[i];
-						console.log(media);
 						let char: Character | undefined = this.characters.find(CH => CH.firstname?.toLowerCase()?.includes(media?.title?.toLowerCase()))
 						if (char) {
 							char.profileURL = `${this.imgApi}/Media/GetMedia?id=${media.id}`
@@ -93,23 +72,23 @@ export class CharactersComponent implements OnInit {
 		let _fisrt: Character = arr?.find(C => C.firstname == 'Marwan') || {}
 		let _second: Character = arr?.find(C => C.firstname == 'Dalilah') || {}
 		let _Third: Character = arr?.find(C => C.firstname == 'Bassem') || {}
-		if (_Third.firstname) {
-			_sortedArr.push(_Third)
-		}
 		if (_fisrt.firstname) {
 			_sortedArr.push(_fisrt)
 		}
 		if (_second.firstname) {
 			_sortedArr.push(_second)
 		}
+		if (_Third.firstname) {
+			_sortedArr.push(_Third)
+		}
 		_sortedArr = _sortedArr.concat(arr?.filter(C => C.firstname != 'Marwan' && C.firstname != 'Dalilah' && C.firstname != 'Bassem') || [])
 		return _sortedArr
 	}
 	onSwiper([swiper]: any) {
-		// console.log(swiper);
+	
 	}
 	onSlideChange() {
-		// console.log('slide change');
+	
 	}
 	isMobile(): boolean {
 		if (window.innerWidth <= 426) {
